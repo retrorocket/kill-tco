d58219d0867fb8e69fc7514478e1151e0bebaa50
retrorocket <unrealenemy@gmail.com>
2012-07-10 19:08:51 +0900

    .attrの戻り値がundefinedだった…

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ $("div[data-item-id]").live("mouseover", function(){
 	$target.each(function() {
 
 		var str = $("span.js-display-url",this).text();
-		if(str != "" && str != null && str != undefined && str.match(/^http/) == null && !$(this).attr("data-display-url")) {
+		if(str != "" && str != null && str != undefined && str.match(/^http/) == null && $(this).attr("data-display-url") == undefined) {
 			var origin = str;
 			$(this).attr("data-display-url",origin);
 			//console.log(origin);
-		} else if (!$(this).attr("data-display-url") && $(this).text().match(/^http/) == null) {
+		} else if ($(this).attr("data-display-url") == undefined && $(this).text().match(/^http/) == null) {
 			var origin = $(this).text();
 			$(this).attr("data-display-url",origin);
 			//console.log(origin);
@@ -28,7 +28,6 @@ $("div[data-item-id]").live("mouseout", function(){
 	var $target = $(this).find("a.twitter-timeline-link");
 	$target.each(function() {
 		if($(this).attr("data-display-url")){
-			//$(this).attr("href", $(this).attr("data-ultimate-url"));
 			$(this).text($(this).attr("data-display-url"));
 		}
 
