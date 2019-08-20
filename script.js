31277adeb64c034a7f0858160ddc89482844d92b
retrorocket <mail@retrorocket.biz>
2019-08-20 21:37:03 +0900

    TwitterのUI変更による更新

--- script.js
+++ script.js
@@ -1,36 +1,6 @@
-$("div[data-item-id]").live("mouseover", function(){
-        var $target = $(this).find("a.twitter-timeline-link").not(".media");
-        $target.each(function() {
-                var str = $("span.js-display-url",this).text();
-                var par  =  $(this).closest("div").hasClass("media");
-
-                if(str != "" && str != null && str != undefined && str.match(/^http/) == null && $(this).attr("data-display-url") == undefined && par == false && $(this).children().is("img")  == false) {
-                        var origin = str + $("span.tco-ellipsis",this).text();
-                        $(this).attr("data-display-url",origin);
-                        //console.log(origin);
-                } else if ($(this).attr("data-display-url") == undefined && $(this).text().match(/^http/) == null) {
-                        var origin = $(this).text();
-                        $(this).attr("data-display-url",origin);
-                        //console.log(origin);
-                }
-                if($(this).attr("data-ultimate-url") != undefined){
-                        $(this).attr("href", $(this).attr("data-ultimate-url"));
-                        $(this).text($(this).attr("data-ultimate-url"));
-                } else if ( $(this).attr("data-expanded-url") != undefined ) {
-                        $(this).attr("href", $(this).attr("data-expanded-url"));
-                        $(this).text($(this).attr("data-expanded-url"));
-                }
-
-        });
+document.querySelector("#react-root").addEventListener("mouseover", (event) => {
+  const target = event.target;
+  if (target.matches("a[role='link']") && target.title) {
+    target.href = target.title;
+  }
 });
-
-$("div[data-item-id]").live("mouseout", function(){
-        var $target = $(this).find("a.twitter-timeline-link").not(".media");
-        $target.each(function() {
-                var par  =  $(this).closest("div").hasClass("media");
-                if($(this).attr("data-display-url") && par  == false && $(this).children().is("img") == false){
-                        $(this).text($(this).attr("data-display-url"));
-                }
-        });
-});
-
