3cbeab67105b645067b1629e59f1af79a6cce522
retrorocket <mail@retrorocket.biz>
2021-02-24 01:32:09 +0900

    UI変更による修正

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.querySelector("#react-root").addEventListener("mouseover", (event) => {
   const target = event.target;
-  if (target.matches("a[role='link']") && target.title) {
-    target.href = target.title;
+  if (target.matches("a[role='link']") && target.href.includes("/t.co/")) {
+    const url = target.textContent;
+    target.href = url.endsWith("…") ? url.substr(0, url.length - 1) : url;
   }
 });
