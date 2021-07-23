5af9366de1d311a72039eafb23ff8769770c2cf3
retrorocket <mail@retrorocket.biz>
2021-07-23 16:29:42 +0900

    1.9 コミットするの忘れてた…

---
+++ README.md
@@ -0,0 +1,4 @@
+# 殲滅t.co
+
+Google Chrome extension that replaces t.co with a regular URL.  
+https://chrome.google.com/webstore/detail/%E6%AE%B2%E6%BB%85tco/jnbhhhjkkdcghmajblmbmdlagndpifji
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.querySelector("#react-root").addEventListener("mouseover", (event) => {
   const target = event.target;
-  if (target.matches("a[role='link']") && target.href.includes("/t.co/")) {
-    const url = target.textContent;
+  const url = target.textContent;
+  if (target.matches("a[role='link']") && target.href.includes("/t.co/") && url.startsWith("http")) {
     target.href = url.endsWith("…") ? url.substr(0, url.length - 1) : url;
   }
 });
